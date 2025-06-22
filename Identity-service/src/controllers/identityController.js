8db5b692a7ae0261eec6d808c7b2596c0b33e2a9
Amit Kumar Sahani <121219168+amit-23-hub@users.noreply.github.com>
2025-06-22 17:03:59 +0530

    added refresh and access token with logout

--- Identity-service/src/controllers/identityController.js
+++ Identity-service/src/controllers/identityController.js
@@ -3,7 +3,7 @@ const logger = require('../utils/Logger.js');
 const User = require('../models/User.js');
 const RefreshToken = require('../models/RefreshToken.js');
 const generateToken = require('../utils/generateToken.js')
-const varifyPassword = require('../models/User.js')
+
 const {ValidateRegistration, Validatelogin} = require('../utils/validation.js');
 
 
@@ -132,14 +132,103 @@ const loginUser = async (req,res)=>{
 }
 
 // refresh token 
+const refreshToken =  async (req, res) => {
+    logger.info('refreshtoken endpoint hit...')
+
+    try {
+        const { refreshToken } = req.body;
+        if (!refreshToken) {
+            logger.warn('Refresh token is required');
+            return res.status(400).json({
+                success: false,
+                message: 'Refresh token is required'
+            });
+        }
+
+        // Check if the refresh token exists in the database
+        const token = await RefreshToken.findOne({ token: refreshToken });
+        if (!token || token.isExpired()< Date.now()) {
+            logger.warn('Invalid refresh token');
+            return res.status(401).json({
+                success: false,
+                message: 'Invalid refresh token'
+            });
+        }
+
+        const user = await User.findById(token.user);
+        if (!user) {
+            logger.warn('User not found for the provided refresh token');
+            return res.status(404).json({
+                success: false,
+                message: 'User not found'
+            });
+        }
+
+        const {accessToken, newRefreshToken} = await generateToken(user);
+
+        // delete the old refresh token
+        await RefreshToken.deleteOne({ token: refreshToken });
+
+        // save the new refresh token
+        const newToken = new RefreshToken({
+            user: user._id,
+            token: newRefreshToken,
+            expiresAt: Date.now() + 7 * 24 * 60 * 60 * 1000 // 7 days
+        });
+        await newToken.save();
 
 
+    }catch (err) {
+        logger.error('refresh token error occured ', err);
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error' });
+    }
+};
 // logout 
+const logoutUser = async (req, res) => {
+    logger.info('logout endpoint hit...');
+    try {
+        const { refreshToken } = req.body;
+        if (!refreshToken) {
+            logger.warn('Refresh token is required');
+            return res.status(400).json({
+                success: false,
+                message: 'Refresh token is required'
+            });
+        }
+        // Check if the refresh token exists in the database
+        const token = await RefreshToken.findOne({ token: refreshToken });
+        if (!token) {
+            logger.warn('Invalid refresh token');
+            return res.status(401).json({
+                success: false,
+                message: 'Invalid refresh token'
+            });
+        }
+        // Delete the refresh token from the database
+        await RefreshToken.deleteOne({ token: refreshToken });
+
+        res.status(200).json({
+            success: true,
+            message: 'User logged out successfully'
+        });
+        
+
+        
+    }catch (err) {
+        logger.error('error occured during logout  ', err);
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error' });
+    }
+};
+
 
 
 module.exports = {
     registerUser,
      loginUser,
-    // refreshToken,
-    // logoutUser
+     refreshToken,
+     logoutUser
 };
